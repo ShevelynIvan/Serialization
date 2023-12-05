1b426d5b82bbba3143819cea028c461778b84437
ShevelynIvan <ivan.shevelyn1@gmail.com>
2023-12-05 13:57:43 +0200

    Добавьте файлы проекта.

---
+++ Serialization/Person.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Serialization
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        [JsonIgnore]
+        public int Weight { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {FirstName}; Last name: {LastName}; Age: {Age}; Weight: {Weight}";
+        }
+    }
+}
---
+++ Serialization/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ivan = new Person() { FirstName = "Ivan", LastName = "Shevelyn", Age = 20, Weight = 62};
+            Console.WriteLine(ivan);
+
+            Person? returned = SerializationAndDeserializationCycle(ivan);
+            Console.WriteLine(returned);
+        }
+
+        static Person? SerializationAndDeserializationCycle(Person person)
+        {
+            string jsonPerson = JsonConvert.SerializeObject(person);
+            Console.WriteLine($"JSON person:\n{jsonPerson}");
+
+            IFormatter formatter = new BinaryFormatter();
+            byte[] serializedData;
+            string deserializedData;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                formatter.Serialize(memoryStream, jsonPerson);
+                serializedData = memoryStream.ToArray();
+            }
+
+            try
+            {
+                using (var memoryStream = new MemoryStream(serializedData))
+                {
+                    deserializedData = (string)formatter.Deserialize(memoryStream);
+                    Console.WriteLine($"Deserialized binary to json person:\n{deserializedData}");
+                }
+
+                Person? deserializedPerson = JsonConvert.DeserializeObject<Person>(deserializedData);
+
+                return deserializedPerson;
+            }
+            catch 
+            {
+                return null;
+            }
+        }
+    }
+}
